dafcef24523209e24f85c39af1eae46e2028ccee
Myrla Rodrigues <myrlarodriguesarq@gmail.com>
2025-06-06 19:24:15 -0300

    Removido marcações de conflito no textarea.js

--- src/components/ui/textarea.js
+++ src/components/ui/textarea.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
+
 export function Textarea({ value, onChange, ...props }) {
   return (
     <textarea
@@ -9,16 +9,4 @@ export function Textarea({ value, onChange, ...props }) {
       {...props}
     />
   );
-=======
-import React from "react";
-export function Textarea({ value, onChange, ...props }) {
-  return (
-    <textarea
-      value={value}
-      onChange={onChange}
-      className="border p-2 rounded w-full"
-      {...props}
-    />
-  );
->>>>>>> 0e0d72ef0ffe35c709d367ba866e0519b1e161e0
 }
